f369e6da74401f3a994ff02ee914d1d8d4486c9b
Edinson Cabral <Edinson.Cabral@mssa.newhorizons.com>
2024-12-02 15:04:12 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Assignment_5_1_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Assignment 5.1.3
+             * 
+             * Given an integer array nums, return true if any value appears 
+             * at least twice in the array, and return false if every element is distinct.
+             * 
+             * Example 1:
+             * Input: nums = [1,2,3,1]
+             * Output: true
+             * 
+             * Example 2:
+             * Input: nums = [1,2,3,4]
+             * Output: false
+             * 
+             * Example 3:
+             * Input: nums = [1,1,1,3,3,4,3,2,4,2]
+             * Output: true
+             */
+
+            int[] nums = { 1, 2, 2, 4, 5, 6 };
+
+            Console.WriteLine( HasDuplicateValue(nums).ToString() );
+        }
+
+        static bool HasDuplicateValue(int[] Nums_)
+        {
+            for (int i = 0; i < Nums_.Length - 1; i++)
+                for (int j = i + 1; j < Nums_.Length; j++)
+                    if (Nums_[i] == Nums_[j])
+                        return true;
+            return false;
+        }
+    }
+}
